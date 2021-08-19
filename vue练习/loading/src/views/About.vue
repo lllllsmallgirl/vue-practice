aa4ebc0fa1ea3efa84e62ce57454a273c12b9976
钟铭 <1257877964@qq.com>
2021-08-19 18:46:54 +0800

    8.19

---
+++ vue练习/loading/src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="block">
+      <el-switch
+        v-model="value"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+      >
+      </el-switch>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: true,
+    };
+  },
+};
+</script>
